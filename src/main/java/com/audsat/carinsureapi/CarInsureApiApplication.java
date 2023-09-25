8dce9bec6ea3ac35f92057e78ebe1dd2ebae25d0
MeloGuilherme <guilhermeamelo.dev@gmail.com>
2023-09-25 13:02:58 -0300

    Iniciando projeto

---
+++ src/main/java/com/audsat/carinsureapi/CarInsureApiApplication.java
@@ -0,0 +1,13 @@
+package com.audsat.carinsureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInsureApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarInsureApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/audsat/carinsureapi/CarInsureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audsat.carinsureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInsureApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
